ce00c61d4b06ca4601290990bf96ae4437923054
Michael Martinez <michaelmartinez4320@yahoo.com>
2023-12-20 19:45:00 -0500

    Sample code to print title of page in terminal

--- src/main/java/com/Michael/Main.java
+++ src/main/java/com/Michael/Main.java
@@ -1,7 +1,14 @@
 package com.Michael;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Document doc = Jsoup.connect("https://essentialsdocs.fandom.com/wiki/Essentials_Docs_Wiki").get();
+            System.out.println(doc.title());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
