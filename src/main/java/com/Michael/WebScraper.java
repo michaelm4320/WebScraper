78138b131f6007252d0f4b6789a59b6dda3ca0aa
Michael Martinez <michaelmartinez4320@yahoo.com>
2023-12-23 22:24:33 -0500

    Resolved Jsoup 404 http error by implementing useragent and referrer.

--- src/main/java/com/Michael/WebScraper.java
+++ src/main/java/com/Michael/WebScraper.java
@@ -8,24 +8,31 @@ public class WebScraper {
     public static void main(String[] args) {
         try {
             String url = "https://essentialsdocs.fandom.com/wiki/Essentials_Docs_Wiki";
-            Document document = Jsoup.connect(url).get();
+            Document document = Jsoup.connect(url)
+                    .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+                    .referrer("http://www.google.com")
+                    .get();
 
-            // Select all 'a' tags within 'li' elements and print their 'href' attributes
             Elements links = document.select("tbody li a");
             for (Element link : links) {
                 String linkUrl = link.attr("abs:href");
-                Document individualPage = Jsoup.connect(linkUrl).get();
+                try {
+                    Document individualPage = Jsoup.connect(linkUrl)
+                            .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+                            .referrer("http://www.google.com")
+                            .get();
 
-                String title = individualPage.select("h1").first().text();
-                Element content = individualPage.getElementById("bodyContent");
+                    String title = individualPage.select("h1").first().text();
+                    Element content = individualPage.getElementById("bodyContent");
 
-                // Call writeToFile from your fileWriting class
-                fileWriting.writeToFile(title, content.text());
+                    fileWriting.writeToFile(title, content.text());
+                } catch (Exception e) {
+                    System.err.println("Error processing link: " + linkUrl);
+                    e.printStackTrace();
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
-
