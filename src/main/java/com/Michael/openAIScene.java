01e4cfbe26eb0fe556666659d41cc8ed7c68ec94
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-06-12 16:00:29 -0400

    update README

--- src/main/java/com/Michael/openAIScene.java
+++ src/main/java/com/Michael/openAIScene.java
@@ -26,6 +26,7 @@ public class openAIScene {
 
         sendMessageArea.setPrefHeight(30);
 
+        Button btnSend = new Button("Submit");
         Button btnFile = new Button("View Files");
         btnFile.setOnAction(e -> {
             primaryStage.setScene(new Scene(app.createMainLayout(primaryStage), 800, 600));
@@ -34,7 +35,7 @@ public class openAIScene {
         HBox hBox = new HBox(10);
         hBox.setPadding(new Insets(15, 12, 15, 12));
         hBox.setStyle("-fx-background-color: #336699;");
-        hBox.getChildren().addAll(btnFile);
+        hBox.getChildren().addAll(btnFile, btnSend);
 
         VBox vBox = new VBox(10);
         vBox.setPadding(new Insets(10));
@@ -42,6 +43,18 @@ public class openAIScene {
 
         rootPane.setTop(hBox);
         rootPane.setLeft(vBox);
+
+        //https://stackoverflow.com/questions/27982895/how-to-use-keyevent-in-javafx-project
+
+        /*btnSend.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.ENTER) {
+                    System.out.println("Enter Pressed");
+                }
+            }
+        });*/
+        //https://stackoverflow.com/questions/42512513/adding-event-listener-to-mainscene-in-javafx-using-fxml
     }
 
     public BorderPane getRootPane() {
